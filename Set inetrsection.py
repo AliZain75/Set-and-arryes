c565c15d66100f4b57950c2c6dcacb946bf5c4b4
AliZain <alizain.vibes@gmail.com>
2025-08-06 16:47:08 +1000

    Set

---
+++ Set inetrsection.py
@@ -0,0 +1,8 @@
+setx = {"green","Blue"} 
+sety = {"Blue","yellow"}
+print("Orginal ste elements")
+print(setx) 
+print(sety) 
+print("\nIntersection of two said sets:")
+setz = setx.intersection(sety)
+print(setz)
